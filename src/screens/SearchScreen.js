7c57434dac9c981b6cbe92f808bf117e6a3adce0
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2020-08-01 19:33:05 -0300

    agregado Axios para hacer el fetch de API

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../componentes/searchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      //agarra el error
+      setErrorMessage("Algo salio mal pá");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
 
   return (
     <View style={estilos.fondo}>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term submited")}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
+      {errorMessage ? <Text style={estilos.error}>{errorMessage}</Text> : null}
+      <Text>Search Screen</Text>
+      <Text> se encontraron {results.length} resultados</Text>
     </View>
   );
 };
@@ -20,6 +46,10 @@ const estilos = StyleSheet.create({
   fondo: {
     backgroundColor: "white",
     height: 800,
+  },
+  error: {
+    color: "red",
+    fontSize: 15,
   },
 });
 
